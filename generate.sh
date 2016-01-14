55989b8a4c6c06fe46fa7828416e919c63178313
Artyom <yom@artyom.me>
2016-01-15 00:29:16 +0300

    [generate.sh] Change paths

--- generate.sh
+++ generate.sh
@@ -2,6 +2,8 @@
 
 today=$(date -u "+%B %-d, %Y")
 
+shortcut_links=$HOME/code/pandoc-contrib/.cabal-sandbox/bin/pandoc-shortcut-links
+
 for f in `find -name "*.md"` 
 do
   if [[ $f == flow/* ]]; then
@@ -11,12 +13,12 @@ do
   outf=${f%.*}
   echo "converting $f"
 
-  pandoc -f markdown -t html5 --mathjax -o $outf -V src:"$f" -V today:"$today" --template=page.template --standalone --css /css.css --filter ~/code/pandoc-contrib/dist/build/pandoc-shortcut-links/pandoc-shortcut-links "$f"
+  pandoc -f markdown -t html5 --mathjax -o $outf -V src:"$f" -V today:"$today" --template=page.template --standalone --css /css.css --filter $shortcut_links "$f"
 done
 
-pandoc -f markdown -t latex -V links-as-notes -V geometry:margin=1.5in -o cv.pdf --filter ~/code/pandoc-contrib/dist/build/pandoc-shortcut-links/pandoc-shortcut-links cv.md
+pandoc -f markdown -t latex -V links-as-notes -V geometry:margin=1.5in -o cv.pdf --filter $shortcut_links cv.md
 
-pandoc -f markdown -t html5 -o cv-plain.html --standalone --filter ~/code/pandoc-contrib/dist/build/pandoc-shortcut-links/pandoc-shortcut-links cv.md
+pandoc -f markdown -t html5 -o cv-plain.html --standalone --filter $shortcut_links cv.md
 
 echo "––––––––––"
 
